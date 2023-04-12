4d0d47d6d952227ff0842b3eaa47cdd72cfa0779
vincentYoung <vinni.xander@gmail.com>
2023-04-12 12:55:50 -0600

    build out weather component

--- app/home/planner/components/LocalDetails.tsx
+++ app/home/planner/components/LocalDetails.tsx
@@ -1,7 +1,9 @@
+import Weather from './localDetails/Weather'
+
 export default function LocalDetails() {
   return (
     <section>
-      {/* weather | weather api */}
+      <Weather />
       {/* exchange rate | exchange api */}
       {/* need-to-know | ai */}
       {/* nice-to-know | ai */}
