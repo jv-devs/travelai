4d5d3a15006f51c87c2521398b7f35bb9cfc899a
John Malapit <johncmalapit@gmail.com>
2023-04-12 16:57:24 -0400

    build ItineraryList component

--- app/home/planner/components/ItineraryList.tsx
+++ app/home/planner/components/ItineraryList.tsx
@@ -1,3 +1,20 @@
+import locationData from '../data/location'
+
+const activities: string[] = locationData.activitySuggestionsList
+
 export default function ItineraryList() {
-  return <div>ItineraryList</div>
+  return (
+    <section className="mx-auto max-w-7xl border-b border-gray-200 bg-white px-6 py-5 sm:px-6 lg:px-8">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        Itinerary List
+      </h3>
+      <ul role="list" className="divide-y divide-gray-200">
+        {activities.map((string, idx) => (
+          <li key={idx} className="px-6 py-4">
+            {string}
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
