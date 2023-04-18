e9f6f7f9859eb04ebc00a242f70c05747dc71fdb
John Malapit <johncmalapit@gmail.com>
2023-04-18 19:35:41 -0400

    update landing page content

--- app/page.tsx
+++ app/page.tsx
@@ -174,25 +174,27 @@ export default function App() {
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    We’re changing the way people connect.
+                    AI-powered travel made simple
+                    <br />
+                    Your ideal trip, in seconds.
                   </h1>
                   <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                    Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-                    quis cupidatat mollit aute velit. Et labore commodo nulla
-                    aliqua proident mollit ullamco exercitation tempor. Sint
-                    aliqua anim nulla sunt mollit id pariatur in voluptate
-                    cillum.
+                    Discover your next adventure with our AI-powered travel app.
+                    Get personalized travel suggestions based on your
+                    preferences and an AI-generated itinerary for your dream
+                    destination. Effortlessly plan your perfect trip and create
+                    unforgettable memories.
                   </p>
                   <div className="mt-10 flex items-center gap-x-6">
-                    <Link
-                      href="#"
+                    <button
+                      onClick={() => signIn()}
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Get started
-                    </Link>
+                    </button>
                     <Link
                       href="#"
-                      className="text-sm font-semibold leading-6 text-gray-900"
+                      className="cursor-not-allowed text-sm font-semibold leading-6 text-gray-900"
                     >
                       Live demo <span aria-hidden="true">→</span>
                     </Link>
