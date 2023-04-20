90fcdeac27f2255081b974df64c36583f2fddfdd
John Malapit <johncmalapit@gmail.com>
2023-04-20 12:13:12 -0400

    update custom loading screen

--- app/home/planner/components/CustomLoadingAnimation.tsx
+++ app/home/planner/components/CustomLoadingAnimation.tsx
@@ -1,82 +1,59 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { vacationFacts } from '../data/vacationFacts'
+import { loadingMessages } from '../data/loadingMessages'
 
-const LoadingDot = {
-  display: 'block',
-  width: '2rem',
-  height: '2rem',
-  backgroundColor: 'black',
-  borderRadius: '50%',
-}
-
-const LoadingContainer = {
-  width: '10rem',
-  height: '5rem',
-  display: 'flex',
-  justifyContent: 'space-around',
-}
-
-const ContainerVariants = {
-  initial: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-  animate: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-}
-
-const DotVariants = {
-  initial: {
-    y: '0%',
-  },
-  animate: {
-    y: '100%',
-  },
-}
-
-const DotTransition = {
-  duration: 0.5,
-  yoyo: Infinity,
-  ease: 'easeInOut',
+const getRandomString = (array: string[]) => {
+  const randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
 }
 
 export default function CustomLoadingAnimation() {
+  const [randomFact, setRandomFact] = React.useState('')
+  const [randomLoadingMessage, setRandomLoadingMessage] = React.useState('')
+
+  useEffect(() => {
+    const fact = getRandomString(vacationFacts)
+    const message = getRandomString(loadingMessages)
+
+    setRandomFact(fact)
+    setRandomLoadingMessage(message)
+  }, [])
+
   return (
-    <div
-      style={{
-        paddingTop: '5rem',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <motion.div
-        style={LoadingContainer}
-        variants={ContainerVariants}
-        initial="initial"
-        animate="animate"
-      >
-        <motion.span
-          style={LoadingDot}
-          variants={DotVariants}
-          transition={DotTransition}
-        />
-        <motion.span
-          style={LoadingDot}
-          variants={DotVariants}
-          transition={DotTransition}
-        />
-        <motion.span
-          style={LoadingDot}
-          variants={DotVariants}
-          transition={DotTransition}
-        />
-      </motion.div>
+    <div className="flex h-full items-center justify-center bg-slate-300">
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 px-4 sm:px-6 lg:px-8">
+        {/* Loader and Message */}
+        <div className="mx-auto max-w-xl rounded px-6 py-4">
+          <p className="text-2xl">{randomLoadingMessage}...</p>
+          <div className="mt-4 flex items-center justify-center">
+            <svg
+              className="-ml-1 mr-3 h-32 w-32 animate-spin text-slate-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v8z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        {/* Fact Component */}
+        <div className="mx-auto max-w-xl rounded bg-white bg-opacity-30 px-6 py-4">
+          <h2 className="text-center italic">Did you know...</h2>
+          <p className="mt-2 text-sm">{randomFact}</p>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ app/home/planner/data/vacationFacts.ts
@@ -0,0 +1,12 @@
+export const vacationFacts = [
+  "The word 'vacation' comes from the Latin word 'vacatio', which means 'freedom' or 'release'.",
+  'The first recorded family vacation was in 1841, when a British civil servant, Thomas Cook, arranged a trip for a group of people from Leicester to Loughborough, about 11 miles away.',
+  'The average American worker gets 10 days of paid vacation per year, while the average European worker gets 20-30 days.',
+  "According to a survey by the U.S. Travel Association, more than half of American workers don't use all their vacation days.",
+  'In 2019, the most visited country in the world was France, with 89 million visitors.',
+  "The Great Barrier Reef in Australia is the world's largest coral reef system and a popular vacation destination for scuba divers.",
+  'Hawaii is the only state in the U.S. that grows coffee commercially.',
+  "The world's largest hotel is the First World Hotel in Malaysia, with over 7,300 rooms.",
+  "The world's most expensive hotel room is the Royal Penthouse Suite at the Hotel President Wilson in Geneva, Switzerland, which costs $80,000 per night.",
+  "The world's tallest waterslide is the Verrückt, located at Schlitterbahn Waterpark in Kansas City, Missouri, with a height of 168 feet and 7 inches.",
+]
