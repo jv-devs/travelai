674ff68aca904a9837c3a2faf59473121635654c
John Malapit <johncmalapit@gmail.com>
2023-04-20 12:26:31 -0400

    add dummy images and image request info

---
+++ app/home/planner/data/images.ts
@@ -0,0 +1,23 @@
+const images = [
+  {
+    imageURL: 'https://example.com/image1.jpg',
+    width: 1000,
+    height: 500,
+    altText: 'A beautiful mountain landscape',
+    userURL: 'https://example.com/user1',
+  },
+  {
+    imageURL: 'https://example.com/image2.jpg',
+    width: 800,
+    height: 600,
+    altText: 'A cute puppy',
+    userURL: 'https://example.com/user2',
+  },
+  {
+    imageURL: 'https://example.com/image3.jpg',
+    width: 1200,
+    height: 800,
+    altText: 'A delicious pizza',
+    userURL: 'https://example.com/user3',
+  },
+]
